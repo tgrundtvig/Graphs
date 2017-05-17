8304d273d83f5521c5a179a91fcbe64f51a3ce13
TryingToImprove <privat.oliver.lassen@gmail.com>
2017-05-17 15:57:34 +0200

    Ensure DAG works when skipping

--- src/Dag/index.test.js
+++ src/Dag/index.test.js
@@ -62,8 +62,6 @@ test("Should return ordered array", () => {
 });
 
 test("Should return ordered array from 5", () => {
-  // There will be a cycle in this graph, and therefor it will not be a valid DAG
-
   const graph = new Graph();
 
   // Nodes
@@ -87,3 +85,36 @@ test("Should return ordered array from 5", () => {
     graph.nodes[2]
   ]);
 });
+
+
+test("Should return ordered array when skipping", () => {
+  const graph = new Graph();
+
+  // Nodes
+  graph.addNode(1);
+  graph.addNode(2);
+  graph.addNode(3);
+  graph.addNode(4);
+  graph.addNode(5);
+  graph.addNode(6);
+  graph.addNode(7);
+
+  // Edges
+  graph.addEdge(1, 2);
+  graph.addEdge(2, 5);
+  graph.addEdge(3, 4);
+  graph.addEdge(4, 5);
+  graph.addEdge(5, 6);
+  graph.addEdge(6, 7);
+
+  const response = getTopologicallyOrder(graph, graph.nodes[1]);
+
+  expect(response).toEqual([
+    graph.nodes[1],
+    graph.nodes[2],
+    graph.nodes[5],
+    graph.nodes[6],
+    graph.nodes[7]
+  ]);
+});
+
